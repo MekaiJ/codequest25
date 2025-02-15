f953c8930f2ddad6ca214a1d0d655d5c26f8a6dd
LiamEarl <liamearl23@gmail.com>
2025-02-15 11:22:44 -0800

    Server Framework

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,21 +1,42 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
 
 class ClientHandler implements Runnable {
     private Socket socket;
-    private ObjectOutputStream in;
+    private ObjectInputStream in;
     private ObjectOutputStream out;
 
-    ClientHandler(Socket socket) {
+    private Object update;
+
+    ClientHandler(Socket socket) throws IOException {
         this.socket = socket;
+        this.in = new ObjectInputStream(socket.getInputStream());
+        this.out = new ObjectOutputStream(socket.getOutputStream());
     }
 
     @Override
     public void run() {
+        try {
+            while (true) {
+                Object fromClient = this.in.readObject();
+                this.update = fromClient;
+            }
+        }catch (Exception e) {}
+    }
 
+    void writeToClient(Object toWrite) throws IOException {
+        this.out.reset();
+        this.out.writeObject(toWrite);
+        this.out.flush();
+    }
+
+    Object getUpdate() {
+        return update;
+    }
+
+    void wipeUpdate() {
+        this.update = null;
     }
 }
