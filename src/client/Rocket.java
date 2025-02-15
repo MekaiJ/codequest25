6ad6fae98efc70a491d81baacd046165c9b46ab7
mjohnson989 <mekai1026@gmail.com>
2025-02-15 12:49:23 -0800

    implement level up system

--- src/client/Rocket.java
+++ src/client/Rocket.java
@@ -12,20 +12,61 @@ public class Rocket implements Serializable {
 
     private String name;
 
-    //maybe add fuel tank
     private int fuel;
+
+    private int xp;
+    private int xpToLevelUp;
 
     private RocketLevel level;
 
     public Rocket(RocketLevel level) {
         this.level = level;
+        this.xp = 0;
     }
 
     private void setStats(RocketLevel level) {
         this.handling = level.getHandling();
         this.durability = level.getDurability();
         this.name = level.getName();
+        this.fuel = level.getFuelCap();
         this.level = level;
+    }
+
+    public void addXp(int xpToAdd) {
+        this.xp += xpToAdd;
+    }
+
+    // Method to check if the rocket should level up
+    private void checkLevelUp() {
+        while (this.xp >= level.getLevelUpXp() && level != RocketLevel.LEVEL_3) {
+            levelUp();
+        }
+    }
+
+    // Method to handle leveling up
+    private void levelUp() {
+        RocketLevel[] levels = RocketLevel.values();
+        int nextLevelIndex = level.ordinal() + 1;
+
+        if (nextLevelIndex < levels.length) {
+            level = levels[nextLevelIndex];
+            this.xp -= level.getLevelUpXp(); // Carry over excess XP
+            updateStats(); // Update rocket stats based on the new level
+            System.out.println("Level up! You are now level " + (level.ordinal() + 1));
+        } else {
+            System.out.println("You are at the maximum level!");
+        }
+    }
+
+    // Method to update rocket stats when leveling up
+    private void updateStats() {
+        System.out.println("Stats updated: Handling=" + level.getHandling() +
+                ", Durability=" + level.getDurability() +
+                ", Name=" + level.getName());
+    }
+
+    public int getXpToNextLevel() {
+        return level.getLevelUpXp();
     }
 
     public String getName() {
--- src/client/RocketLevel.java
+++ src/client/RocketLevel.java
@@ -3,18 +3,22 @@ package client;
 import java.io.Serializable;
 
 public enum RocketLevel implements Serializable {
-    LEVEL_1(3, 3, "Rust Bucket"),
-    LEVEL_2(5, 5, "The Comet"),
-    LEVEL_3(7, 7, "Time Traveler");
+    LEVEL_1(3, 3, "Rust Bucket", 10, 100),
+    LEVEL_2(5, 5, "The Comet", 50, 300),
+    LEVEL_3(7, 7, "Time Traveler", 100, 600);
 
     private final int handling;
     private final int durability;
     private final String name;
+    private final int fuelCap;
+    private final int levelUpXp;
 
-    RocketLevel(int handling, int durability, String name) {
+    RocketLevel(int handling, int durability, String name, int fuelCap, int levelUpXp) {
         this.handling = handling;
         this.durability = durability;
         this.name = name;
+        this.fuelCap = fuelCap;
+        this.levelUpXp = levelUpXp;
     }
 
     public int getHandling() {
@@ -24,6 +28,10 @@ public enum RocketLevel implements Serializable {
     public int getDurability() {
         return durability;
     }
+
+    public int getFuelCap() {return fuelCap;}
+
+    public int getLevelUpXp() {return levelUpXp;}
 
     public String getName() {
         return name;
