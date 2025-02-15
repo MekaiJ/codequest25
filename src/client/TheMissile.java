ef9971eab7104aeaf4a6f7e9a154eb45a774744d
mjohnson989 <mekai1026@gmail.com>
2025-02-15 11:57:43 -0800

    create rocket assets

--- src/client/TheMissile.java
+++ src/client/TheMissile.java
@@ -1,7 +1,6 @@
 package client;
 //level 1 rocket
 public class TheMissile extends Rocket{
-
     public TheMissile() {
         //TODO: change placeholder values to something different
         super(5, 3, "The Missile");
