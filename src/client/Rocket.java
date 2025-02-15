f292da16bbe5a8245b149e234477a0dc417e7f2b
mjohnson989 <mekai1026@gmail.com>
2025-02-15 14:55:55 -0800

    setter methods for rocket

--- src/client/Rocket.java
+++ src/client/Rocket.java
@@ -144,4 +144,11 @@ public class Rocket implements Serializable {
         return y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
